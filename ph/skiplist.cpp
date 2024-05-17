fe2e46648d99bb5c1bf793b64eda80b821d30059
cgmin <m47341@gmail.com>
2024-05-17 12:55:39 +0900

    skiplist add need test

--- ph/skiplist.cpp
+++ ph/skiplist.cpp
@@ -23,6 +23,21 @@ size_t getRandomLevel()
 	return rand() % MAX_LEVEL;
 }
 
+void Skiplist_Node::setLevel(size_t l)
+{
+	delete next;
+	next = new std::atomic<Skiplist_Node*>[l+1];
+	built = 0;
+}
+
+void Skiplist_Node::setLevel()
+{
+	level = getRandomLevel();
+	delete next;
+	next = new std::atomic<Skiplist_Node*>[level+1];
+	built = 0;
+}
+
 void Skiplist::init()
 {
 //	node_pool_list = (Tree_Node**)malloc(sizeof(Skiplist_Node*) * NODE_POOL_LIST_SIZE);
@@ -49,6 +64,11 @@ void Skiplist::init()
 	for (i=0;i<MAX_LEVEL;i++)
 		start_node->next[i] = end_node;
 
+	Skiplist_Node* node = alloc_sl_node();
+	node->level = 0;
+	node->key = 0;
+	start_node->next[0] = node;
+	node->next[0] = end_node;
 
 }
 
@@ -86,12 +106,19 @@ Skiplist_Node* Skiplist::alloc_sl_node()
 			printf("no space for node!\n");
 		++node_pool_list_cnt;
 		node_pool_list[node_pool_list_cnt] = new Skiplist_Node[NODE_POOL_SIZE];//(Skiplist_Node*)malloc(sizeof(Skiplist_Node) * NODE_POOL_SIZE);
+		node_pool_cnt = 0;
 	}
+
+	Skiplist_Node* node = &node_pool_list[node_pool_list_cnt][node_pool_cnt];
+	node->lock = 0;
+	node->delete_lock = 0;
+	node->setLevel();
 
 //	if (node_pool_cnt < NODE_POOL_SIZE)
 	{
+		node_pool_cnt++;
 		at_unlock2(node_alloc_lock);
-		return &node_pool_list[node_pool_list_cnt][node_pool_cnt++];
+		return node;
 	}
 }
 
@@ -102,5 +129,108 @@ void Skiplist::free_sl_node(Skiplist_Node* node)
 	node_free_head = node;
 	at_unlock2(node_alloc_lock);
 }
+
+Skiplist_Node* Skiplist::find_node(size_t key,Skiplist_Node** prev,Skiplist_Node** next) // what if max
+{
+	Skiplist_Node* node = start_node;
+	int i;
+	for (i=MAX_LEVEL;i>=0;i--)
+	{
+		while(true)
+		{
+			while(node->next[i].load()->delete_lock);
+			if (node->next[i].load()->key >= key)
+				node = node->next[i];
+			else
+				break;
+		}
+		prev[i] = node;
+		next[i] = node->next[i];
+
+	}
+	return node;
+}
+
+void Skiplist::delete_node(Skiplist_Node* node,Skiplist_Node** prev,Skiplist_Node** next)
+{
+	size_t key = node->key;
+	at_lock2(node->delete_lock);
+	while(1)
+	{
+//		if (at_try_lock2(node->delete_lock))
+		{
+			if (delete_node_with_fail(node,prev,next))
+				return;
+//			at_unlock2(node->delete_lock);
+		}
+//		else
+//			printf("impossible use lock\n");
+
+		node = find_node(key,prev,next);
+//		if (node->key != key)
+//			return;
+	}
+}
+
+bool Skiplist::delete_node_with_fail(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next)
+{
+	if (try_at_lock2(node->lock) == 0)
+		return false;
+	
+	Skiplist_Node* pn;
+	Skiplist_Node* nn;
+	int i;
+
+	for (i=node->level;i>=0;i--)
+	{
+		if (prev[i]->delete_lock) // prev first...
+			return false;
+//		while(node->next[i]->delete_lock);
+//		if (at_try_lock2(prev[i]->lock) == 0)
+//			continue;
+		pn = prev[i]->next[i];
+		nn = node->next[i];
+		if (pn != node)
+			return false;
+		if (prev[i]->next[i].compare_exchange_strong(pn,nn) == false)
+			return false;
+		node->level--;
+	}
+	return true;
+
+}
+
+bool Skiplist::insert_node_with_fail(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next)
+{
+	// level already
+	int i;
+
+	Skiplist_Node *pn;
+
+	for (i=node->built;i<=node->level;i++)
+	{
+		if (prev[i]->delete_lock) // not gonna happend
+			return false;
+		pn = prev[i]->next[i];
+		if (pn != next[i])
+			return false;
+		node->next[i] = next[i];
+		if (prev[i]->next[i].compare_exchange_strong(pn,node->next[i]) == false)
+			return false;
+		node->built++;
+	}
+	return true;
+}
+
+void Skiplist::insert_node(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next)
+{
+	while(1)
+	{
+		if (insert_node_with_fail(node,prev,next))
+			return;
+		find_node(node->key,prev,next);
+	}
+}
+
 
 }
--- ph/skiplist.h
+++ ph/skiplist.h
@@ -1,18 +1,43 @@
 #include<vector>
+#include<atomic>
 
 namespace PH
 {
 
+#if 0
+class Skiplist_Node;
+class AtomicPointer
+{
+	public:
+	AtomicPointer() {};
+//	AtomicPointer() = default;
+//	AtomicPointer(AtomicPointer* ap) {snp = ap->snp;}
+	AtomicPointer(AtomicPointer &ap) {snp = ap.snp.load();}
+//	template<typename T> AtomicPointer(T const&) {}
+//	AtomicPointer& AtomicPointer::operator=(const AtomicPointer &rhs) {snp = rhs.snp; return *this;} 
 
+	std::atomic<Skiplist_Node*> snp=NULL;
+};
+#endif
 //struct Skiplist_Node
 class Skiplist_Node
 {
 	public:
+	~Skiplist_Node() { delete next; }
 	size_t key;
 //	Skiplist_Node* node_p; // tree node or leaf
-	std::vector<Skiplist_Node*> next;
+//	std::vector<std::atomic<Skiplist_Node*>> next;
+//	std::vector<AtomicPointer> next;
+	std::atomic<Skiplist_Node*> *next = NULL;
+
 	size_t level;
+	size_t built;
 //	Tree_Node* next;
+	std::atomic<uint8_t> lock;
+	std::atomic<uint8_t> delete_lock;
+
+	void setLevel();
+	void setLevel(size_t l);
 };
 
 class Skiplist
@@ -35,6 +60,13 @@ class Skiplist
 	Skiplist_Node* alloc_sl_node();
 	void free_sl_node(Skiplist_Node* node);
 
+	Skiplist_Node* find_node(size_t key,Skiplist_Node** prev,Skiplist_Node** next);
+	bool delete_node_with_fail(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next);
+	void delete_node(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next);
+	bool insert_node_with_fail(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next);
+	void insert_node(Skiplist_Node* node, Skiplist_Node** prev,Skiplist_Node** next);
+
+//	void split(
 };
 
 
